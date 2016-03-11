8b6b6622960734206c54abf18b2581ae5f553253
vstoneofficial <yokoyama@vstone.co.jp>
2016-03-11 15:41:11 +0900

    1.2.9
    Change-Id: Ic77415428a031cbf0e419eddd453a2f9f402316c

--- src/java_run.sh
+++ src/java_run.sh
@@ -1,7 +1,9 @@
-classpath=".\
-:/home/vstone/lib/*\
+classpath=".:/home/vstone/lib/*\
 :/home/vstone/vstonemagic/*\
+:/usr/local/share/OpenCV/java/*\
 "
 
-echo "java -classpath $classpath -Dfile.encoding=UTF8 $1"
-java -classpath "$classpath" -Dfile.encoding=UTF8 $1
+OPTION="-Dfile.encoding=UTF8 -Djava.library.path=/usr/local/share/OpenCV/java/"
+
+echo "java -classpath $classpath $OPTION $1"
+java -classpath "$classpath" $OPTION $1
--- src/jp/vstone/sotasample/FaceAuthenticationSample2.java
+++ src/jp/vstone/sotasample/FaceAuthenticationSample2.java
@@ -36,37 +36,37 @@ public class FaceAuthenticationSample2 {
 		CSotaMotion motion = new CSotaMotion(mem);
 		CRoboCamera cam = new CRoboCamera("/dev/video0", motion);
 		sotawish = new MotionAsSotaWish(motion);
-		
+
 		SpeechRecog speechrec = new SpeechRecog(motion);
-		
+
 		if(mem.Connect()){
 			//Sota仕様にVSMDを初期化
 			motion.InitRobot_Sota();
-			
+
 			CRobotUtil.Log(TAG, "Rev. " + mem.FirmwareRev.get());
-			
+
 			//サーボモータを現在位置でトルクOnにする
 			CRobotUtil.Log(TAG, "Servo On");
 			motion.ServoOn();
-			
+
 			//すべての軸を動作
 			pose = new CRobotPose();
 			pose.SetPose(new Byte[] {1   ,2   ,3   ,4   ,5   ,6   ,7   ,8}	//id
-					,  new Short[]{0   ,-900,0   ,900 ,0   ,0   ,0   ,0}				//target pos
+			,  new Short[]{0   ,-900,0   ,900 ,0   ,0   ,0   ,0}				//target pos
 					);
 			//LEDを点灯（左目：赤、右目：赤、口：Max、電源ボタン：赤）
 			pose.setLED_Sota(Color.BLUE, Color.BLUE, 255, Color.GREEN);
-			
+
 			motion.play(pose, 500);
 			CRobotUtil.wait(500);
-			
+
 			//笑顔推定有効
 			cam.setEnableSmileDetect(true);
 			//顔検索有効
 			cam.setEnableFaceSearch(true);
 			//フェイストラッキング開始
 			cam.StartFaceTraking();
-			
+
 			int detectcnt = 0;
 			while(true){
 				FaceDetectResult result = cam.getDetectResult();
@@ -79,7 +79,7 @@ public class FaceAuthenticationSample2 {
 				}
 
 				if(detectcnt > 3){	
-					FaceUser user = cam.getUser(2000);
+					FaceUser user = cam.getUser(result);
 					if(user != null){
 						if(user.isNewUser()){
 							sotawish.Say("はじめまして。あなたの名前はなんですか？");
@@ -88,9 +88,25 @@ public class FaceAuthenticationSample2 {
 								sotawish.Say("あなたの名前は," + name + ",でまちがいない？");
 								String yesorno= speechrec.getYesorNo(10000, 3);
 								if(yesorno != null && yesorno.contains(SpeechRecog.ANSWER_YES)){
+									
 									user.setName(name);
-									cam.addUser(user);
-									CPlayWave.PlayWave(TextToSpeechSota.getTTSFile("オッケー！"+ name +"の顔は覚えたよ。") , true);
+									if(cam.addUser(user)){
+										CPlayWave.PlayWave(TextToSpeechSota.getTTSFile("オッケー！"+ name +"の顔は覚えたよ。") , true);
+									}
+									else{
+										CPlayWave.PlayWave(TextToSpeechSota.getTTSFile("正面じゃなかったから、登録できなかったよ。もう一度撮影するから、じっとしててね。") , true);
+									}
+									result = cam.getDetectResult();
+									user = cam.getUser(result);
+									if(user.isNewUser()){
+										user.setName(name);
+										if(cam.addUser(user)){
+											CPlayWave.PlayWave(TextToSpeechSota.getTTSFile("オッケー！"+ name +"の顔は覚えたよ。") , true);
+										}
+										else{
+											CPlayWave.PlayWave(TextToSpeechSota.getTTSFile("登録できなかったよ。") , true);
+										}
+									}
 								}
 								else if(yesorno != null && yesorno.contains(SpeechRecog.ANSWER_NO)){
 									sotawish.Say("ちがった。ごめんね。");
